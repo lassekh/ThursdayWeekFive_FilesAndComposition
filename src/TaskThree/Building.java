618b63b9ed378c808ab239c735cd6ae378543339
lassekh <lasse.kh.92@gmail.com>
2023-03-16 10:26:53 +0100

    Upload completed Thursday Tasks

--- src/TaskThree/Building.java
+++ src/TaskThree/Building.java
@@ -3,14 +3,26 @@ package TaskThree;
 import java.util.ArrayList;
 
 public class Building {
-    private ArrayList<Room> rooms = new ArrayList<>(); //Add final to variable
+    private final ArrayList<Room> rooms; //Add final to variable
     private int numberOfBathrooms;
     private int numberOfFloors;
     private boolean isOfficeBuilding;
-    Building(ArrayList<Room> rooms, int bathrooms, int floors, boolean isOfficeBuilding){
+    public Building(ArrayList<Room> rooms, int bathrooms, int floors, boolean isOfficeBuilding){
         this.rooms = rooms;
         this.numberOfBathrooms = bathrooms;
         this.numberOfFloors = floors;
         this.isOfficeBuilding = isOfficeBuilding;
+    }
+    public ArrayList<Room> getRooms(){
+        return this.rooms;
+    }
+    public int getNumberOfBathrooms(){
+        return this.numberOfBathrooms;
+    }
+    public int getNumberOfFloors(){
+        return this.numberOfFloors;
+    }
+    public boolean getIsOfficeBuilding(){
+        return this.isOfficeBuilding;
     }
 }
--- src/TaskThree/Main.java
+++ src/TaskThree/Main.java
@@ -9,9 +9,31 @@ public class Main {
         officeBuilding.add(new Room(4,16,8));
         officeBuilding.add(new Room(1,1,0));
 
-        Building office = new Building(officeBuilding, 2,2,true);
-    }
-    public static void countLampsInBuilding(Building b){
+        Building office = new Building(officeBuilding, 2,3,true);
 
+        int totalLamps = countLampsInBuilding(office);
+        System.out.println(totalLamps);
+
+        boolean buildingType = isNormal(office);
+        System.out.println(buildingType);
+    }
+    public static int countLampsInBuilding(Building b){
+        //Count number of lamps
+        //Get rooms in building...
+        //Get number of lamps in each room...
+        //Plus number of lamps
+        int lampsNum = 0; //Variable to store number of lamps
+        for(Room r : b.getRooms()){
+            lampsNum += r.getNumberOfLamps();
+        }
+        return lampsNum;
+    }
+    public static boolean isNormal(Building b){
+        if(b.getNumberOfFloors() > b.getRooms().size()) {
+            return true;
+        } else {
+            System.out.println("This is an odd building");
+            return false;
+        }
     }
 }
